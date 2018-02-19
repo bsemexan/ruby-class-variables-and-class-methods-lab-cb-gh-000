2785690e40f1f0a739fd7003ba67c7ed8ca74e10
Berry  Semexan <bsemexan@gmail.com>
2018-02-19 15:38:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,21 +17,23 @@ class Song
   def self.count
     @@count
   end
-  
+
   def self.genres
     @@genres.uniq
   end
-  
+
   def self.artists
     @@artists.uniq
   end
 
   def self.genre_count
     genre_hash = {}
-    @@genres.each {|g_count| genre_hash[g_count] = @@genres.count(genre)}
+    @@genres.each {|g_count| genre_hash[g_count]= @@genres.count(genre)}
     genre_hash
   end
 
   def self.artist_count
+    artist_hash = {}
+    @@artists.each {|h_count| artist_hash[h_count]= @@artists.count(artist)}
   end
 end
